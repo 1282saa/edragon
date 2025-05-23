0d1304473c46be709c4e96ca76ae190c9fa96d04
User Name <user@example.com>
2025-05-23 11:49:53 +0900

    Fix Dockerfile for Cloud Run deployment

--- server.py
+++ server.py
@@ -475,4 +475,4 @@ if __name__ == '__main__':
     
     # 서버 실행
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port)
